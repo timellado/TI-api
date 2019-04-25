2be1f6df13e43183213c2407a7e37b7dc27b391d
SebaCardenas <sacardenas@uc.cl>
2019-04-25 00:30:36 -0400

    create db with rake db:create && rake db:migrate

--- db/migrate/20190424174821_create_orders.rb
+++ db/migrate/20190424174821_create_orders.rb
@@ -1,7 +1,7 @@
 class CreateOrders < ActiveRecord::Migration[5.1]
   def change
     create_table :orders do |t|
-      t.int :Id_o
+      t.integer :Id_o
       t.string :Sku
       t.integer :Cantidad
       t.string :Almacen_id
