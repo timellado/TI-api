bf61ffc341598a8eb694631e22339d4557d3899e
Dalal15 <dcchahuan@uc.cl>
2019-04-29 11:18:06 -0400

    destroy all

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,4 @@
 class Product < ApplicationRecord
-    has_and_belongs_to_many :ingredients
-    has_and_belongs_to_many :groups
+    has_and_belongs_to_many :ingredients, dependent: :destroy
+    has_and_belongs_to_many :groups, dependent: :destroy
 end
