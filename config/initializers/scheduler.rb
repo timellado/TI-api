a0cffa15b40855d2438d166599ec57cf414eeb46
CRISTOBAL GOMARA <cgomara@uc.cl>
2019-04-30 19:53:19 -0400

    sch v6
    .join

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -4,12 +4,24 @@ require 'rufus-scheduler'
 #include ProductSKU
 
 
-
-
 scheduler = Rufus::Scheduler.new
-job = scheduler.every '1s' do
-puts "hello world"
+
+
+scheduler.every '2h',  :first_in => 5 do
+  ApplicationRecord.keep_minimum_stock
 end
 scheduler.join
-#   end
+
+# scheduler.every '20s' do
+#   lista = ProductSKU.get_sku_product()
+#   lista.each do |i|
+#     p i
+#     Logica.sacar_de_despacho(i[0], 200)
+scheduler2 = Rufus::Scheduler.new
+scheduler2.every '20m', :first_in => 2 do
+  ApplicationRecord.clean
+end
+scheduler2.join
+
+
 # end
