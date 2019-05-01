a7de76438100d0941f60552c0fbc45b27ae39d2f
CRISTOBAL GOMARA <cgomara@uc.cl>
2019-04-30 20:07:36 -0400

    schd v7

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -4,24 +4,35 @@ require 'rufus-scheduler'
 #include ProductSKU
 
 
-scheduler = Rufus::Scheduler.new
+if defined?(::Rails::Server)
+	puts "Partiendo scheduler"
 
 
-scheduler.every '2h',  :first_in => 5 do
-  ApplicationRecord.keep_minimum_stock
+
+  job_sftp = Rufus::Scheduler.new(:max_work_threads => 1)
+  job_sftp.every '10m' :first_in => 2 do
+    ApplicationRecord.clean
+		puts "Termina de limpiar"
+  end
+
 end
-scheduler.join
+
+
+
+#scheduler = Rufus::Scheduler.new
+
+
+#scheduler.every '2h',  :first_in => 5 do
+ # ApplicationRecord.keep_minimum_stock
+#end
+#scheduler.join
 
 # scheduler.every '20s' do
 #   lista = ProductSKU.get_sku_product()
 #   lista.each do |i|
 #     p i
 #     Logica.sacar_de_despacho(i[0], 200)
-scheduler2 = Rufus::Scheduler.new
-scheduler2.every '20m', :first_in => 2 do
-  ApplicationRecord.clean
-end
-scheduler2.join
+
 
 
 # end
