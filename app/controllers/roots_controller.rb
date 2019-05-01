f1d3336c9281ad018a1f5cb8197e7b262e3e91ec
SebaCardenas <sacardenas@uc.cl>
2019-04-30 21:05:57 -0400

    debug view ready

--- app/controllers/roots_controller.rb
+++ app/controllers/roots_controller.rb
@@ -1,8 +1,10 @@
 require 'bodega'
 require 'variable'
+require 'product_sku'
 class RootsController < ApplicationController
   include Bodega
   include Variable
+  include ProductSKU
   def index
     @results_root = JSON.parse(Bodega.all_almacenes().to_json)
     @cocina = Variable.v_cocina()
@@ -11,5 +13,16 @@ class RootsController < ApplicationController
     @pulmon = Variable.v_pulmon()
     @inventario1 = Variable.v_inventario1()
     @inventario2 = Variable.v_inventario2()
+
+    @sku_product_list = ProductSKU.get_sku_product()
+    @lista_stock = Inventory.get_inventory
+
+    @results_despacho = JSON.parse(Bodega.get_skus_almacen(@despacho).to_json)
+    @results_recepcion = JSON.parse(Bodega.get_skus_almacen(@recepcion).to_json)
+    @results_pulmon = JSON.parse(Bodega.get_skus_almacen(@pulmon).to_json)
+    @results_inventario1 = JSON.parse(Bodega.get_skus_almacen(@inventario1).to_json)
+    @results_inventario2 = JSON.parse(Bodega.get_skus_almacen(@inventario2).to_json)
+    @results_cocina = JSON.parse(Bodega.get_skus_almacen(@cocina).to_json)
+
   end
 end
