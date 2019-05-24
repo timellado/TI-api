88098f1faf71ab33aac4060388d178a089d8c102
josefinabengoa <jbengoa1@uc.cl>
2019-05-23 21:58:51 -0400

    ftp listo

--- lib/ftp.rb
+++ lib/ftp.rb
@@ -1,13 +1,14 @@
 require 'net/sftp'
 require 'httparty'
 
+
 module FTP
 
   def self.get_id()
   sftp = Net::SFTP.start('fierro.ing.puc.cl', 'grupo10_dev', password: 'kljBJ73njcHGKh1')  ## necesitamos dos conexiones
   Net::SFTP.start('fierro.ing.puc.cl', 'grupo10_dev', password: 'kljBJ73njcHGKh1') do |entries|
         entries.dir.foreach('/pedidos/') do |entry|
-          puts entry
+          puts "SFTP recibido"
           if entry.name.include?("xml")
           		date_ingreso = entry.name.split('.xml').join
           		sftp.file.open("/pedidos" + "/" + entry.name, "r") do |f|
@@ -16,17 +17,14 @@ module FTP
                 content_qty = nil
                 while !f.eof?
                   content = f.gets
-
                   # obtengo los ids
                   if content.include?("id")
                     content = content.split('<')[2]
                     content = content.split('>')[1]
                     content_id = content
-
                     self.get_oc_data(content_id)
                   end #end if
                 end #end while
-
               end #end if
             end
           end
@@ -34,29 +32,25 @@ module FTP
       end
 
     def  self.save_data(results)
-      puts "guardando data"
-      puts results['_id']
-      puts results['_id'].class
+      puts "Revisando si se debe guardar en base de datos"
       if Ordencompra.where(oc_id:results['_id']).count == 0
-        puts "guardando en la bd"
+        puts "Guardando en la base de datos"
         @orden_compra = Ordencompra.create!(oc_id: results['_id'],
                                           sku: results['sku'].to_s,
                                           cliente: results['cliente'],
                                           fechaEntrega: results['fechaEntrega'],
                                           cantidad:  results['cantidad'],
                                           estado: results["estado"],
-
                                           )
-        puts "ya guardado"
 end
 end
+
     def self.get_oc_data(id)
-      puts "pedir oc info al profe"
       response = HTTParty.get("https://integracion-2019-dev.herokuapp.com/oc/"+'obtener/'+id,
       :headers =>{'Content-Type' => 'application/json'})
-      puts "ya pedida info"
-      results = JSON.parse(response.parsed_response)
-      puts results
+      puts "Request API OC hecha"
+      results = JSON.parse(response.to_s)
+      results = results[0]
       self.save_data(results)
     end
 end
