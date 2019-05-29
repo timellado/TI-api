72bd4339ceff27312144f3ac357925b6d8abf2f1
CRISTOBAL GOMARA <cgomara@uc.cl>
2019-05-29 17:08:26 -0400

    kill cron
    descativar los cron

--- config/schedule.rb
+++ config/schedule.rb
@@ -6,22 +6,22 @@ set :environment, "development"
 set :output, "log/cron.log"
 
 
-every 7.minutes do
-    rake "clean"
-end
+#every 7.minutes do
+ #   rake "clean"
+#end
 
-every 51.minutes do
-    rake "keep_min_stock"
-end
+#every 51.minutes do
+ #   rake "keep_min_stock"
+#end
 
-every 11.minutes do
-  rake  "obtain_ftp_order"
-end
+#every 11.minutes do
+ ## rake  "obtain_ftp_order"
+#end
 
-every 23.minutes do
-    rake "clean_order_register"
-end
+#every 23.minutes do
+#    rake "clean_order_register"
+#end
 
-every 13.minutes do
-    rake "filtrar_ftp"
-end
+#every 13.minutes do
+ ##   rake "filtrar_ftp"
+#end
