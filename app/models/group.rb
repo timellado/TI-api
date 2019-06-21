15f056ae5ef8d3d32ab268d6f894a47bf4bf749f
SebaCardenas <sacardenas@uc.cl>
2019-06-21 18:33:32 -0400

    add self in Group

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,8 +1,8 @@
 require 'logica'
 class Group < ApplicationRecord
     has_and_belongs_to_many :products, dependent: :destroy
-    
-    def enviar_materia(sku,cantidad,almacen_destino, oc)
+
+    def self.enviar_materia(sku,cantidad,almacen_destino, oc)
         Logica.mover_productos_a_despacho_y_despachar(sku,cantidad,almacen_destino, oc)
     end
 end
